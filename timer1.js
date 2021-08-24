5ec8af244ea9bc699d5246f02cb190482fc43efd
amaninders <amandeolofficial@gmail.com>
2021-08-24 19:32:07 +0000

    implemented edge case scenarios and revised the code

--- timer1.js
+++ timer1.js
@@ -1,8 +1,9 @@
 const args = process.argv.slice(2);
 
 const timer = (array) =>  {
-  array.forEach(timeStamp =>
-    setTimeout(() => {
+
+	array.filter(x => x > 0 && !isNaN(x)).forEach(timeStamp =>
+		setTimeout(() => {
       process.stdout.write('\x07');
     }, timeStamp * 1000));
 };
